3a19be6652a0cbd3712a6079cc85243e41218f43
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-05-29 19:25:02 -0600

    week-1

--- src/CarouselServiceProvider.php
+++ src/CarouselServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Tuto1902\Carousel;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Tuto1902\Carousel\Commands\CarouselCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class CarouselServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,7 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->name('carousel')
+            ->hasViews();
     }
 }
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Tuto1902\Carousel\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
