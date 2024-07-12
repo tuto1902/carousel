4d896b8b831df258515ca2bf579cd25a6dfd5e95
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-07-11 19:21:31 -0600

    Fixed carousel size
    Wrap the entire carousel in a flex container in order for the frame to respect the size.

--- resources/views/components/carousel/index.blade.php
+++ resources/views/components/carousel/index.blade.php
@@ -19,25 +19,27 @@ $autoPlayOptions = [
     'stopOnMouseEnter' => true
 ];
 @endphp
-<div x-data="carouselData()" x-init="init(@js($options)@if($autoplay == 'true'), {{ $autoplay }}, @js($autoPlayOptions)@endif)" class="relative w-full max-w-xl" role="region" aria-roledescription="carousel">
-    <!-- Slides Viewport -->
-    <div x-ref="viewport" class="overflow-hidden">
-        <!-- Container -->
-        <div @class([ "flex $size", 'flex-col' => $orientation == 'vertical' ])>
-            <!-- Slides -->
-            {{ $slot }}
+<div class="flex m-16">
+    <div x-data="carouselData()" x-init="init(@js($options)@if($autoplay == 'true'), {{ $autoplay }}, @js($autoPlayOptions)@endif)" class="relative max-w-xl" role="region" aria-roledescription="carousel">
+        <!-- Slides Viewport -->
+        <div x-ref="viewport" class="overflow-hidden">
+            <!-- Container -->
+            <div @class([ "flex $size", 'flex-col' => $orientation == 'vertical' ])>
+                <!-- Slides -->
+                {{ $slot }}
+            </div>
         </div>
+        <x-carousel::carousel.buttons.previous>
+            <svg class="lucide lucide-arrow-left size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m12 19-7-7 7-7" />
+                <path d="M19 12H5" />
+            </svg>
+        </x-carousel::carousel.buttons.previous>
+        <x-carousel::carousel.buttons.next>
+            <svg class="lucide lucide-arrow-right size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14" />
+                <path d="m12 5 7 7-7 7" />
+            </svg>
+        </x-carousel::carousel.buttons.next>
     </div>
-    <x-carousel::carousel.buttons.previous>
-        <svg class="lucide lucide-arrow-left size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="m12 19-7-7 7-7" />
-            <path d="M19 12H5" />
-        </svg>
-    </x-carousel::carousel.buttons.previous>
-    <x-carousel::carousel.buttons.next>
-        <svg class="lucide lucide-arrow-right size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M5 12h14" />
-            <path d="m12 5 7 7-7 7" />
-        </svg>
-    </x-carousel::carousel.buttons.next>
 </div>
