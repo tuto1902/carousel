c6d8eb4fe02c4ae9a8a41e375b0b977cba7cf619
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-07-15 17:08:29 -0600

    toned down buttons
    In order to avoid having to deal with custom themes, I'm toning down the button colors to just whites and grays

--- resources/views/components/carousel/buttons/base.blade.php
+++ resources/views/components/carousel/buttons/base.blade.php
@@ -1,14 +1,14 @@
 @php
 
 $classes = [
-    'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium text-white ring-offset-gray-950 transition-colors focus-visible:outline-none focus-visible:ring-2 dark:focus-visible:ring-amber-400/50 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-amber-500/50 dark:border-amber-400/50 bg-amber-600 dark:bg-amber-500 hover:bg-amber-500 dark:hover:bg-amber-400 absolute size-8 rounded-full',
+    'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium dark:text-white ring-offset-gray-950/5 dark:ring-offset-white/10 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-gray-950/5 dark:border-white/10 bg-white dark:bg-gray-950 hover:bg-gray-50 dark:hover:bg-gray-900 absolute size-8 rounded-full',
     $attributes->get('class')
 ]
 
 @endphp
-<button 
+<button
     @click="{{ $attributes->get('@click') }}"
-    type="button" 
+    type="button"
     @class($classes)
 >
     {{ $slot }}
